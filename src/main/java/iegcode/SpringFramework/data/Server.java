68694d6360a633cc79af24b4f8fdfe6d5a83e0f0
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-03-01 22:20:28 +0700

    update

--- src/main/java/iegcode/SpringFramework/data/Server.java
+++ src/main/java/iegcode/SpringFramework/data/Server.java
@@ -3,7 +3,6 @@ package iegcode.SpringFramework.data;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 
 @Slf4j
 public class Server {
