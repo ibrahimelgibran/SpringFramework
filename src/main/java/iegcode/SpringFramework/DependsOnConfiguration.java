933d1f3873082ea815bbea896c124a1d8e169866
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-03-01 20:17:06 +0700

    update

--- src/main/java/iegcode/SpringFramework/DependsOnConfiguration.java
+++ src/main/java/iegcode/SpringFramework/DependsOnConfiguration.java
@@ -6,11 +6,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Lazy;
 
 @Configuration
 @Slf4j
 public class DependsOnConfiguration {
 
+    @Lazy
     @Bean
     @DependsOn({
             "bar"
---
+++ src/main/java/iegcode/SpringFramework/ScopeConfiguration.java
@@ -0,0 +1,37 @@
+package iegcode.SpringFramework;
+
+import iegcode.SpringFramework.data.Bar;
+import iegcode.SpringFramework.data.Foo;
+import iegcode.SpringFramework.scope.DoubletonScope;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Slf4j
+@Configuration
+public class ScopeConfiguration {
+
+    @Bean
+    @Scope("prototype")
+    public Foo foo(){
+        log.info("Create new Foo");
+        return new Foo();
+    }
+
+    @Bean
+    public CustomScopeConfigurer customScopeConfigurer(){
+        CustomScopeConfigurer configurer = new CustomScopeConfigurer();
+        configurer.addScope("doubleton", new DoubletonScope());
+        return configurer;
+    };
+
+    @Bean
+    @Scope("doubleton")
+    public Bar bar(){
+        log.info("Create new Bar");
+        return new Bar();
+    }
+
+}
